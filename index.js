ebc4c44d75d6d38f982ce01d17b73a06f5df9694
Xennia Contreras <Xenbydesign@gmail.com>
2023-10-30 23:01:10 -0500

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 62;
+let num2 = 1;
+let multiply = num1 * num2;
+
+let random = Math.floor((Math.random() * 10) + 1);
+
+let num3 = 5
+let num4 = 14
+
+let mod = num4 % num3;
+
+let max = Math.max(1, 2, 20);
